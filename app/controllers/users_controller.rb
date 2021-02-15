7db4c727f451bf13b9cbd79a0baa06ee9d297c0d
floup42 <pousseurdec@gmail.com>
2021-02-16 00:05:51 +0100

    ALL_WOKING_FOR_INSTANT

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
+  def index
+    @user = User.all
+  end
+
+  def show
+    @events = @user.events
+  end
+
+  # def new
+  # end
+
+  # def edit
+  # end
+
+  # def create
+  # end
+
+  # def update
+  # end
+
+  def destroy
+    @user.destroy
+    redirect_to root_path
+  end
+  
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).params(:email, :password)
+  end
+end
